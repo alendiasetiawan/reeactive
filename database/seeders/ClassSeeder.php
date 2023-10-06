c4d0bd846e39716be9bc6fb2eab939dd40b02e36
alendia21 <idsimponi@gmail.com>
2023-10-06 19:23:56 +0700

    Add Seeder

--- database/seeders/ClassSeeder.php
+++ database/seeders/ClassSeeder.php
@@ -19,24 +19,28 @@ class ClassSeeder extends Seeder
                 'start_time' => '08:10:00',
                 'end_time' => '09:10:00',
                 'day' => 'Senin, Rabu, Jumat',
+                'class_status' => 'Open'
             ],
             [
                 'coach_code' => '85774827925',
                 'start_time' => '05:30:00',
                 'end_time' => '06:30:00',
                 'day' => 'Selasa, Kamis, Jumat',
+                'class_status' => 'Open'
             ],
             [
                 'coach_code' => '85774827925',
                 'start_time' => '07:00:00',
                 'end_time' => '08:00:00',
                 'day' => 'Selasa, Kamis, Jumat',
+                'class_status' => 'Open'
             ],
             [
                 'coach_code' => '85774827925',
                 'start_time' => '09:00:00',
                 'end_time' => '10:00:00',
                 'day' => 'Selasa, Kamis, Jumat',
+                'class_status' => 'Open'
             ],
         ]);
     }
