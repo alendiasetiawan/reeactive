0be942a067239e35db43370b3f4325b4a5b7b187
alendia21 <idsimponi@gmail.com>
2023-10-11 17:37:55 +0700

    Fix : Route Ganti Password

--- resources/views/layouts/elements/sidebars/member_sidebar.blade.php
+++ resources/views/layouts/elements/sidebars/member_sidebar.blade.php
@@ -87,8 +87,8 @@
                 </div>
             </li>
 
-            <li class="menu {{ Route::is('member::ganti_password') ? 'active' : '' }}">
-                <a wire:navigate href="{{ route('member::ganti_password') }}" aria-expanded="false" class="dropdown-toggle">
+            <li class="menu {{ Route::is('ganti_password') ? 'active' : '' }}">
+                <a wire:navigate href="{{ route('ganti_password') }}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
                         <span>Password</span>
