b3d523a0b5c399d0d546f31a98cbad701ea5438d
Alendia Setiawan <alendia.setiawan@gmail.com>
2024-04-15 06:58:01 +0700

    Update countdown di landing page

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -9,16 +9,17 @@ use Illuminate\Http\Request;
 class LandingPageController extends Controller
 {
     public function index(BatchService $batchService) {
+        $batchQuery = $batchService->batchQuery();
         $data = [
             'title' => 'Online Coaching Untuk Muslimah',
-            'batch' => $batchService->batchQuery(),
+            'batch' => $batchQuery,
             'privatePrograms' => Program::privatePrograms(),
             'buddyPrograms' => Program::buddyPrograms(),
             'smallPrograms' => Program::smallPrograms(),
             'specialPrograms' => Program::specialPrograms(),
             'largePrograms' => Program::largePrograms(),
             'nutrionPrograms' => Program::nutrionPrograms(),
-            'openDate' => '2023-12-25 09:00:00'
+            'openDate' => $batchQuery->start_date,
         ];
 
         return view('welcome', $data);
