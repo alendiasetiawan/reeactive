2a53255ec41281cb109e9346a18aa14862258caf
Alendia Setiawan <alendia.setiawan@gmail.com>
2024-10-09 22:40:46 +0700

    Fix : disable @use directive due to issue in hosting server

--- resources/views/livewire/admin/registrations/registered-by-referral.blade.php
+++ resources/views/livewire/admin/registrations/registered-by-referral.blade.php
@@ -1,6 +1,4 @@
 <div>
-    @use('App\Helpers\CurrencyHelper')
-
     <x-items.breadcrumb>
         <x-slot name="mainPage" href="/">Dashboard</x-slot>
         <x-slot name="currentPage">Member Claim Referral</x-slot>
@@ -63,7 +61,7 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-circle"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
                                 </a>
                                 <br/>
-                                Total Claim : {{ CurrencyHelper::formatRupiah($member->referralRegistrations->sum('discount')) }}
+                                Total Claim : {{ \App\Helpers\CurrencyHelper::formatRupiah($member->referralRegistrations->sum('discount')) }}
                             </p>
                             <div class="progress-data">
                                 <div class="progress-info">
