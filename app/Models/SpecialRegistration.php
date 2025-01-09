fb49f3606e0538a99ee1a26647c208177901bd59
Alendia Setiawan <alendia.setiawan@gmail.com>
2025-01-09 09:21:28 +0700

    Fix : link join group WA on member dashboard

--- app/Models/SpecialRegistration.php
+++ app/Models/SpecialRegistration.php
@@ -34,7 +34,7 @@ class SpecialRegistration extends Model
         return self::join('programs', 'special_registrations.program_id', '=', 'programs.id')
             ->join('coaches', 'special_registrations.coach_id', '=', 'coaches.id')
             ->join('classes', 'special_registrations.class_id', '=', 'classes.id')
-            ->select('special_registrations.*','programs.program_name', 'coaches.nick_name', 'classes.day', 'classes.start_time', 'classes.end_time')
+            ->select('special_registrations.*','programs.program_name', 'coaches.nick_name', 'classes.day', 'classes.start_time', 'classes.end_time', 'classes.link_wa')
             ->where('special_registrations.member_code', $memberCode)
             ->orderBy('special_registrations.id', 'desc')
             ->limit($limitData)
