f8afe52740a9670807152abb98bca131d4fbe721
Alendia Setiawan <alendia.setiawan@gmail.com>
2025-01-10 20:54:20 +0700

    Fix : typo in form lepasan

--- resources/views/layouts/vuexy-blank.blade.php
+++ resources/views/layouts/vuexy-blank.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="Onlince coaching for muslimah">
     <title>{{ $title }} - Reeactive | Fit For Deen</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('template/src/assets/img/favicon.ico') }}"/>
-    <link href="https://fonts.googleapis.com/css2?family=Dongle:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
     <link rel="stylesheet" type="text/css" href="{{ asset('style/app-assets/vendors/css/vendors.min.css') }}">
